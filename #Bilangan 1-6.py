c8fc1a921270944751f3a5b32345c07678351caa
Reelcoo <146726239+Reelcoo@users.noreply.github.com>
2024-11-08 09:08:31 +0700

    Add files via upload

---
+++ #Bilangan 1-6.py
@@ -0,0 +1,26 @@
+#Bilangan 1-6
+i = 1
+while i <= 6:
+    if i % 2 == 0:
+        print(f"{i} adalah bilangan genap")
+    else:
+        print(f"{i} adalah bilangan ganjil")
+    i += 1
+
+
+
+#Bilangan genap 1-6
+i = 1
+while i <= 6:
+    if i % 2 == 0:
+        print(f"{i} adalah bilangan genap")
+    i += 1
+
+
+
+#Bilangan ganjil 1-6
+i = 1
+while i <= 6:
+    if i % 2 != 0:
+        print(f"{i} adalah bilangan ganjil")
+    i += 1
