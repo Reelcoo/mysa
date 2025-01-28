5d2a78498e05a529443fbd19f29eda37122a3160
Reelcoo <146726239+Reelcoo@users.noreply.github.com>
2025-01-29 06:31:32 +0700

    Add files via upload

---
+++ # Program Biodata Muhammad Bilal Isha Musa.py
@@ -0,0 +1,16 @@
+# Program Biodata Muhammad Bilal Isha Musa
+def tampilkan_biodata():
+    nama = "Muhammad Bilal Isha Musa"
+    umur = 21
+    alamat = "Lemburstiu, Sukabumi"
+    email = "ishamusa5555@gmail.com"
+
+    print("\n---- Biodata Diri ----")
+    print(f"Nama: {nama}")
+    print(f"Usia: {umur}")
+    print(f"Alamat: {alamat}")
+    print(f"Email: {email}")
+    print("----------------------")
+
+# Menampilkan biodata
+tampilkan_biodata()
